538e7d98f10cc3b086fccb668d7b8328c96e02af
mohitccem <mohit.ccem@gmail.com>
2019-06-09 18:55:50 +0530

    Crud Repository for Product class

---
+++ src/main/java/com/target/retail/repository/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.target.retail.repository;
+
+import com.target.retail.model.Product;
+//import org.springframework.data.cassandra.repository.CassandraRepository;
+//import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product,Integer>
+{
+    //@Query(value = "select * from Product where id=?0")
+    Product findByProductId(int productId);
+}
