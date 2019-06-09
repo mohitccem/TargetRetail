8399546c0ce07bfe3b85fd69d3deb18153883c94
mohitccem <mohit.ccem@gmail.com>
2019-06-09 18:54:57 +0530

    Service Interface.

---
+++ src/main/java/com/target/retail/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.target.retail.service;
+
+import com.target.retail.dto.ProductDTO;
+import com.target.retail.model.Product;
+
+public interface ProductService {
+
+    ProductDTO getProduct(int id);
+
+    ProductDTO updatePrice(Product prod);
+
+    ProductDTO createProduct(Product product);
+
+}
